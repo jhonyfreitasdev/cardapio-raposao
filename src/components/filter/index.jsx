8ce32492659c2780be096e3bda8541db50940ebb
Jhony Freitas <jhony00._@hotmail.com>
2023-11-23 20:56:16 -0300

    Criando componente Filter

---
+++ src/components/filter/index.jsx
@@ -0,0 +1,13 @@
+import "./index.sass"
+
+export const Filter = () => {
+    return(
+        <div className="container"> 
+            <ul className="list">
+                <li className="item"></li>
+                <li className="item"></li>
+                <li className="item"></li>
+            </ul>
+        </div>
+    )
+}
