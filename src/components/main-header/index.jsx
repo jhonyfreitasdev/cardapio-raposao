a0508584568492bbc8bc9e6184bcbc3168ff28ad
Jhony Freitas <jhony00._@hotmail.com>
2023-11-23 20:56:30 -0300

    Criando componente Main Header

---
+++ src/components/main-header/index.jsx
@@ -0,0 +1,10 @@
+import LogoImage from "../../assets/images/logo-raposao.jpg"
+
+
+export const MainHeader = () => {
+    return (
+        <>
+            <img src={ LogoImage } alt="Imagem do logo" />
+        </>
+    )
+}
