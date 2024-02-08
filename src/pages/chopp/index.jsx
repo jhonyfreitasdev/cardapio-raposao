e3a31dfca72fdba034b11de58daa2e1799d6756c
Jhony Freitas <jhony00._@hotmail.com>
2024-02-08 18:39:21 -0300

    Criando componente de chopp

--- src/pages/chopp/index.jsx
+++ src/pages/chopp/index.jsx
@@ -1,7 +1,15 @@
-export const Chopp = () => {
+import { NavBar } from "../../components/nav-bar";
+import { Menu } from "../../components/menu";
+import "./index.sass"
+
+export const ChoppPage = () => {
     return (
-        <>
-        
-        </>
+        <div className="chopp-container">
+            <NavBar></NavBar>
+            <div className="chopp-content">
+                <h2 className="chopp-title"> Cervejas </h2>
+                <Menu></Menu>
+            </div>
+        </div>
     )
 }
