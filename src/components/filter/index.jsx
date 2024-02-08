4fb7b57eec44835cd2d439eb296383e03a7e41ab
Jhony Freitas <jhony00._@hotmail.com>
2024-02-08 18:32:10 -0300

    Criando componente de filtro

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom"
 import "./index.sass"
 
 export const Filter = () => {
     return(
-        <div className="container"> 
+        <div className="container-filter"> 
             <ul className="list">
-                <li className="item"></li>
-                <li className="item"></li>
-                <li className="item"></li>
+                <li className="item"><Link to="/food">Comidas</Link></li>
+                <li className="item"><Link to="/chopp">Cervejas</Link></li>
+                <li className="item"><Link to="/drinks">Drinks</Link></li>
             </ul>
         </div>
     )
