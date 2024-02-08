229c0d14adfd220e738a94d384c538ec3d4e7f90
Jhony Freitas <jhony00._@hotmail.com>
2024-02-08 18:39:42 -0300

    Criando componente de food

--- src/pages/food/index.jsx
+++ src/pages/food/index.jsx
@@ -1,7 +1,15 @@
-export const Food = () => {
+import { NavBar } from "../../components/nav-bar";
+import { Menu } from "../../components/menu";
+import "./index.sass"
+
+export const FoodPage = () => {
     return (
-        <>
-        
-        </>
+        <div className="food-container">
+            <NavBar></NavBar>
+            <div className="food-content">
+                <h2 className="food-title"> Comidas </h2>
+                <Menu></Menu>
+            </div>
+        </div>
     )
-}
+};
