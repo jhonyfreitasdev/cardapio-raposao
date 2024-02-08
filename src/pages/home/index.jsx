896ee7a115c810a2bbdd9d62b2870383ed45adad
Jhony Freitas <jhony00._@hotmail.com>
2024-02-08 18:40:01 -0300

    Criando página home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,12 @@
-import { MainHeader } from "../../components/main-header";
-import { Filter } from "../../components/filter";
- 
+import { Header } from "../../components/header";
+import { MenuBar } from "../../components/menu-bar";
+import "./index.sass";
+
 export const Home = () => {
     return (
-        <>
-            <MainHeader />
-            <Filter />
-        </>
+        <div className="container-home">
+            <Header />
+            <MenuBar />
+        </div>
     )
 } 
