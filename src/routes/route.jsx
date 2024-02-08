cbdbef0555a58c58d2680d16135a4269fca987ee
Jhony Freitas <jhony00._@hotmail.com>
2024-02-08 18:40:10 -0300

    Criando rotas do projeto

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { Home } from "../pages/home"
+import { FoodPage } from "../pages/food"
+import { ChoppPage } from "../pages/chopp"
+import { DrinkPage } from "../pages/drink"
 
 export const AppRoutes = () => (
     <BrowserRouter>
         <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/food" element={<FoodPage />} />
+            <Route exact path="/chopp" element={<ChoppPage />} />
+            <Route exact path="/drinks" element={<DrinkPage />} />
         </Routes>
     </BrowserRouter>
 );
