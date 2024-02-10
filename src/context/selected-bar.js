c0e3a7945b39603da767e67e962a97c32e036daa
Jhony Freitas <jhony00._@hotmail.com>
2024-02-09 23:33:15 -0300

    Criando contexto da página selecionada

--- src/context/selected-bar.js
+++ src/context/selected-bar.js
@@ -1,13 +1,13 @@
 import { createContext, useState } from "react";
 
-export const selectedPageContext = createContext()
+export const selectedPageContext = createContext();
 
 export const SelectedPageProvider = ({children}) => {
-    const [page, setPage] = useState('')
+    const [page, setPage] = useState('');
 
     return ( 
         <selectedPageContext.Provider value={{page, setPage}}>
             {children}
         </selectedPageContext.Provider>
     )
-}
+};
