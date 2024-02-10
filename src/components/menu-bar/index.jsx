34e5c1c79e07e02b92906df722158479612dee0c
Jhony Freitas <jhony00._@hotmail.com>
2024-02-09 23:32:47 -0300

    Adicionando links no menu

--- src/components/menu-bar/index.jsx
+++ src/components/menu-bar/index.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
-import "./index.sass"
+import { useContext } from "react";
+import { selectedPageContext } from "../../context/selected-bar";
+import "./index.sass";
 
 export const MenuBar = () => {
+    const { setPage } = useContext(selectedPageContext);
+
     return(
         <div className="container-filter"> 
             <ul className="list">
-                <li className="item"><Link to="/food">Comidas</Link></li>
-                <li className="item"><Link to="/chopp">Cervejas</Link></li>
-                <li className="item"><Link to="/drinks">Drinks</Link></li>
+                <li className="item"><Link to="/food" id="food" onClick={e => setPage(e.target.id)}> Comidas </Link></li>
+                <li className="item"><Link to="/chopp" id="chopp" onClick={e => setPage(e.target.id)}> Cervejas </Link></li>
+                <li className="item"><Link to="/drink" id="drink" onClick={e => setPage(e.target.id)}> Drinks </Link></li>
             </ul>
         </div>
     )
-}
+};
