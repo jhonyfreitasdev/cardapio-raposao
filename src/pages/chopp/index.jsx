5768c975afd3d623379e606b738019290a358ae9
Jhony Freitas <jhony00._@hotmail.com>
2024-02-09 23:34:09 -0300

    Ajustes de classe

--- src/pages/chopp/index.jsx
+++ src/pages/chopp/index.jsx
@@ -1,15 +1,15 @@
 import { NavBar } from "../../components/nav-bar";
 import { Menu } from "../../components/menu";
-import "./index.sass"
+import "./index.sass";
 
 export const ChoppPage = () => {
     return (
         <div className="chopp-container">
             <NavBar></NavBar>
             <div className="chopp-content">
-                <h2 className="chopp-title"> Cervejas </h2>
+                <h2 className="title"> Cervejas </h2>
                 <Menu></Menu>
             </div>
         </div>
     )
-}
+};
--- src/pages/food/index.jsx
+++ src/pages/food/index.jsx
@@ -1,13 +1,13 @@
 import { NavBar } from "../../components/nav-bar";
 import { Menu } from "../../components/menu";
-import "./index.sass"
+import "./index.sass";
 
 export const FoodPage = () => {
     return (
         <div className="food-container">
             <NavBar></NavBar>
             <div className="food-content">
-                <h2 className="food-title"> Comidas </h2>
+                <h2 className="title"> Comidas </h2>
                 <Menu></Menu>
             </div>
         </div>
